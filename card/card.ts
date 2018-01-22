7c592ff8d08070d72eef61ec923fa2244986966e
Cookies <fredspivock@gmail.com>
2018-01-21 21:54:54 -0500

    added inspect

--- card/card.ts
+++ card/card.ts
@@ -2,11 +2,51 @@ import { CardType } from "./card-type";
 import { Suit } from "./suit";
 
 export class Card {
+    private cardSuit: Suit;
+    private cardType: CardType;
+
     constructor(suit: Suit, cardType: CardType) {
-        this.suit = suit;
+        this.cardSuit = suit;
         this.cardType = cardType;
     }
 
-    public cardType: CardType;
-    public suit: Suit;
+    //suit getters
+    get suit(): Suit {
+        return this.cardSuit;
+    }
+
+    get suitName(): string {
+        return this.cardSuit.name;
+    }
+
+    get suitKey(): string {
+        return this.cardSuit.key;
+    }
+
+
+    //cardType getters
+    get type(): CardType {
+        return this.cardType;
+    }
+
+    get cardTypeName(): string {
+        return this.cardType.name
+    }
+
+    get typeValue(): number {
+        return this.cardType.value;
+    }
+
+    get typeSymbol(): string {
+        return this.cardType.symbol;
+    }
+
+    //get combined card info
+    get face(): string {
+        return this.cardType.symbol + this.cardSuit.key;
+    }
+
+    get cardString(): string {
+        return this.cardType.name + ' of ' + this.cardSuit.name + 's'
+    }
 }
--- hearts.ts
+++ hearts.ts
@@ -1,5 +1,108 @@
-import { DeckService } from "./service/deck.service";
+import {Card} from "./card/card";
+import {SUITS, CARD_TYPES, JOKER} from './constant/deck.constant'
 
-let deckService = new DeckService();
 
-console.log(deckService.shuffle(deckService.getDecks(1, 0)));
+export class Deck {
+
+    private decks: Card[];
+
+    constructor(numberOfDecks: number = 1, numOfJokers: number = 0) {
+        this.decks = this.createDecks(numberOfDecks, numOfJokers);
+        this.shuffle();
+    }
+
+    get cards() {
+        return this.decks;
+    }
+
+    draw(numberOfCards: number = 1, from: 'top' | 'bottom' | 'random' = 'top'): Card[] {
+        if(numberOfCards < 1 || numberOfCards > this.decks.length) {
+            console.error();
+            return;
+        }
+
+        let cards: Card[] = [];
+
+        for (let i = 0; i < numberOfCards; i++) {
+            if(from === 'top') {
+                cards.push(this.decks.pop())
+            }
+            else if(from === 'bottom') {
+                cards.push(this.decks.shift());
+            }
+            else {
+                let randomCardIndex = Math.floor(Math.random() * this.decks.length);
+                cards.push(this.decks.splice(randomCardIndex, 1)[0]);
+            }
+        }
+        return cards;
+    }
+
+    //Look at cards
+    inspect(numberOfCards:number = 1, from: 'top' | 'bottom' | 'random' = 'top'): Card[] {
+        if(numberOfCards < 1 || numberOfCards > this.decks.length) {
+            console.error();
+            return;
+        }
+
+        let cards: Card[] = [];
+        let copyOfDeck: Card[] = [...this.decks];
+
+        for (let i = 0; i < numberOfCards; i++) {
+            if(from === 'top') {
+                cards.push(this.decks[i])
+            }
+            else if(from === 'bottom') {
+                cards.push(this.decks[this.decks.length - 1]);
+            }
+            else {
+                let randomCardIndex = Math.floor(Math.random() * this.decks.length);
+                cards.push(copyOfDeck.splice(randomCardIndex, 1)[0]);
+            }
+        }
+        return cards;
+    }
+
+    private createDecks(numberOfDecks: number = 1, numJoker: number = 0) {
+        let decks: Card[] = [];
+
+        if (numberOfDecks < 1 || numberOfDecks > 100) {
+            console.error();
+            return;
+        }
+
+        for (let i = 0; i < numberOfDecks; i++) {
+            decks = decks.concat(this.buildDeck(numJoker));
+        }
+        return decks;
+    }
+
+    shuffle() {
+        let copy: Card[] = [],
+        index: number;
+        while (this.decks.length > 0) {
+            index = Math.floor(Math.random() * this.decks.length);
+            copy.push(this.decks.splice(index, 1)[0]);
+        }
+        this.decks.push(...copy);
+
+        return this.decks;
+    }
+
+
+    private buildDeck(numJoker: number) {
+        let deck = [];
+        for (let suit of SUITS) {
+            for (let cardType of CARD_TYPES) {
+                deck.push(new Card(suit, cardType))
+            }
+        }
+
+        for (let i = 0; i < numJoker; i++) {
+            deck.push(new Card(JOKER.suit, JOKER.cardType))
+        }
+
+        return deck;
+    }
+
+}
